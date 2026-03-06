e1cc35e0f44017d7e0b22cbe8c142500fd592ebf
ruiyang01 <rui.yang1@protonmail.com>
2026-03-06 22:42:25 +0100

    support more stock symbols

--- services/data-fetcher/src/config.py
+++ services/data-fetcher/src/config.py
@@ -11,7 +11,7 @@ class Config:
     ])
     
     stock_symbols: list[str] = field(default_factory=lambda: [
-        "NVDA", "IREN", "SATS"
+        "NVDA", "IREN", "SATS", "MRVL"
     ])
     
     crypto_interval: int = 5
